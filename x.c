fc0ef63516f0bf364ed20f943060a97b15247743
Kohei Hosokawa <hosokawa@complex.ist.hokudai.ac.jp>
2019-01-15 17:06:44 +0900

    :sparkles: Shift + Mouse Scroll

--- x.c
+++ x.c
@@ -409,6 +409,7 @@ bpress(XEvent *e)
 {
 	struct timespec now;
 	MouseShortcut *ms;
+    MouseKey *mk;
 	int snap;
 
 	if (IS_SET(MODE_MOUSE) && !(e->xbutton.state & forceselmod)) {
@@ -423,6 +424,14 @@ bpress(XEvent *e)
 			return;
 		}
 	}
+
+    for (mk = mkeys; mk < mkeys + LEN(mkeys); mk++) {
+      if (e->xbutton.button == mk->b
+          && match(mk->mask, e->xbutton.state)) {
+        mk->func(&mk->arg);
+        return;
+      }
+    }
 
 	if (e->xbutton.button == Button1) {
 		/*
